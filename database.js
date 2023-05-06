26d553c915278361e3ec9ea853a3f5c2fb7fec1b
Vinay Kumar <98196841+VINAYKUMARKUNDER@users.noreply.github.com>
2023-05-06 14:06:11 +0530

    Update database.js

--- database.js
+++ database.js
@@ -1,12 +1,23 @@
 
+const mysql = require('mysql2');
 
-const { Sequelize } = require("sequelize");
-const conn = new Sequelize("railway", "root", "0J0HaTJcmEzm4HkDPAsd", {
-  host: "localhost",
-  dialect: "mysql",
+ const conn= mysql.createConnection({
+    host:'containers-us-west-104.railway.app',
+    user:'root',
+    password:'0J0HaTJcmEzm4HkDPAsd',
+    database:'railway'
 });
 
+module.exports=conn;
+
+
+// const { Sequelize } = require("sequelize");
+// const conn = new Sequelize("railway", "root", "0J0HaTJcmEzm4HkDPAsd", {
+//   host: "localhost",
+//   dialect: "mysql",
+// });
 
 
 
-module.exports = conn;
+
+// module.exports = conn;
