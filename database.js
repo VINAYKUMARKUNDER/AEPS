7194ed371d5eb71ffd33dfcccc147da509df7959
Vinay Kumar <98196841+VINAYKUMARKUNDER@users.noreply.github.com>
2023-05-11 18:05:32 +0530

    database connectvity

--- database.js
+++ database.js
@@ -1,2 +1,10 @@
 
-const sequelize = require('sequelize')
+const { Sequelize } = require("sequelize");
+const conn = new Sequelize("aeps", "root", "Vinay@1313", {
+  host: "localhost",
+  dialect: "mysql",
+});
+
+conn.query('select * from fc', (req, res)=>{
+    console.log('connext');
+})
--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-console.log('ff')
+
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+
+
+
+const app = express();
+
+app.use(cors());
+app.use(bodyParser.json());
+
+
+app.listen(3000,()=>{
+    console.log('port is running...');
+});
