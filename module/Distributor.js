446b886b80afb51eff357816d669050dd450ab99
Vinay Kumar <98196841+VINAYKUMARKUNDER@users.noreply.github.com>
2023-05-11 18:30:23 +0530

    Cart.belongsTo(Medicine, {   foreignKey: { allowNull: false },   onDelete: CASCADE, }); module created

---
+++ module/Distributor.js
@@ -0,0 +1,89 @@
+const { Sequelize, DataTypes } = require("sequelize");
+const db = require("../database");
+
+const Distributor = db.define(
+  "Distributor",
+  {
+    fc_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    country: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    district: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    house_no: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    pincode: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    state: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    street: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    sub_district: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    thana: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    village: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    area_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    bio: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    mobile: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  },
+
+  {
+    tableName: "Distributor",
+    timestamps: false,
+  }
+);
+
+const FC = require('./FC')
+
+Distributor.belongsTo(FC, {
+    foreignKey: { allowNull: false },
+    onDelete: "CASCADE",
+  });
+
+module.exports = Distributor;
