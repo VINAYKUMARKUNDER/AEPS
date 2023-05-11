d7be18c0aa3fc3514b482e8cc643e79b53696b50
Vinay Kumar <98196841+VINAYKUMARKUNDER@users.noreply.github.com>
2023-05-11 19:30:46 +0530

    fc controller crud opration

--- routers/fcController.js
+++ routers/fcController.js
@@ -24,12 +24,41 @@ router.get("/:id", async (req, res) => {
   }
 });
 
-
 // create new entry
 router.post("/", async (req, res) => {
   try {
     const data = await FcModule.create(req.body);
     res.status(201).json((msg = "new data create successfully..."));
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+// update entry
+router.put("/:id", async (req, res) => {
+  try {
+    const data = await FcModule.update(req.body, {
+      where: {
+        fc_id: req.params.id,
+      },
+    });
+    if (data[0] == 0) res.status(200).json("already updated...");
+    else res.status(200).json("update successfully....");
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+// deleted enrty by id
+router.delete("/:id", async (req, res) => {
+  try {
+    const data = await FcModule.destroy({
+      where: {
+        fc_id: req.params.id,
+      },
+    });
+    if (data == 1) res.status(200).json("deleted successfully");
+    else res.status(200).json("data not found...");
   } catch (error) {
     res.status(500).json(error);
   }
