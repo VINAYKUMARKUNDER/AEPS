185087e69661e1d54a9ae12166a661b1e21c5b2e
Vinay Kumar <98196841+VINAYKUMARKUNDER@users.noreply.github.com>
2023-05-12 11:11:33 +0530

    Support controller created

---
+++ module/Support.js
@@ -0,0 +1,33 @@
+const { DataTypes } = require("sequelize");
+const db = require("../database");
+
+const Support = db.define(
+  "Support",
+  {
+    support_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: false,
+      autoIncrement: false,
+    },
+    query: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+    support_type: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  },
+  {
+    tableName: "Support",
+    timestamps: false,
+  }
+);
+
+const Retailer = require("./Retailer");
+Support.belongsTo(Retailer, {
+  foreignKey: id,
+});
+
+module.exports = Support;
