98a5a2c48c554943e033cc07de6f5fd4248af509
Vinay Kumar <98196841+VINAYKUMARKUNDER@users.noreply.github.com>
2023-05-12 12:17:22 +0530

    transaction h

---
+++ module/Transaction_hist.js
@@ -0,0 +1,38 @@
+const { DataTypes } = require("sequelize");
+const db = require("../database");
+
+const Transaction_history = db.define(
+  "Transaction_history",
+  {
+    transaction_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    },
+    description: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    amount: {
+      type: DataTypes.DOUBLE,
+      allowNull:false
+    },
+    transaction_date: {
+      type: DataTypes.DATE,
+      allowNull: false
+    },
+    transaction_type: {
+      type: DataTypes.STRING,
+      allowNull: false
+    }
+  },
+  { 
+    tableName: "Transaction_history", 
+    timestamps: false 
+  }
+);
+
+const Retailer = require('./Retailer')
+Transaction_history.belongsTo(Retailer, {foreignKey:id});
+
+module.exports = Transaction_history;
+
