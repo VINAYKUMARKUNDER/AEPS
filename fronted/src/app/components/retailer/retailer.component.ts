9dae335f3f86129f443a86662d966e3cc88a85d3
Vinay Kumar <98196841+VINAYKUMARKUNDER@users.noreply.github.com>
2023-05-24 13:02:56 +0530

    fc modify

--- fronted/src/app/components/retailer/retailer.component.ts
+++ fronted/src/app/components/retailer/retailer.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -11,12 +12,23 @@ export class RetailerComponent implements OnInit{
 
   }
 
-  constructor(){}
+  URL: string = 'http://localhost:3000/api/v1/';
+  constructor(private http:HttpClient ){}
 
   status:string='create';
 
   changeStatus(status:string){
     this.status=status;
+  }
+
+
+  fcCreate(value:any){
+    this.http.post(`${this.URL}retailer/`, value).subscribe({
+      next:res=>{alert(res)
+      console.log(res)},
+      error:err=>{alert(err)
+      console.log(err)}
+    })
   }
 
 }
