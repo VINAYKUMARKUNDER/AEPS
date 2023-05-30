7a2ca86e65f67dacd2b27eef44c15073fa7cc5c0
Vinay Kumar <98196841+VINAYKUMARKUNDER@users.noreply.github.com>
2023-05-30 17:36:14 +0530

    activity

--- Server/module/Activity/ActivityController.js
+++ Server/module/Activity/ActivityController.js
@@ -4,65 +4,18 @@ const db = require("../../database");
 const ActivityModule = require("../module/Activity");
 
 // get all entry
-router.get("/", async (req, res) => {
-  try {
-    const data = await ActivityModule.findAll();
-    res.status(200).json(data);
-  } catch (error) {
-    res.status(500).json(error);
-  }
-});
+router.get("/", );
 
 // get entry by id
-router.get("/:id", async (req, res) => {
-  try {
-    const data = await ActivityModule.findByPk(req.params.id);
-    if (!data) res.status(200).json("data not found with id: ", req.params.id);
-    else res.status(200).json(data);
-  } catch (error) {
-    res.status(500).json(error);
-  }
-});
+router.get("/:id",);
 
 // create new entry
-router.post("/", async (req, res) => {
-  try {
-    const data = await ActivityModule.create(req.body);
-    res.status(201).json("new entry created successfully...");
-  } catch (error) {
-    res.status(500).json(error);
-  }
-});
+router.post("/", );
 
 // updated entry by id
-router.put("/:id", async (req, res) => {
-  try {
-    const data = await ActivityModule.update(req.body, {
-      where: {
-        activity_id: req.params.id,
-      },
-    });
-    if (data[0] == 1) res.status(200).json("updated successfully...");
-    else res.status(200).json("already updated...");
-  } catch (error) {
-    res.status(500).json(error);
-  }
-});
+router.put("/:id", );
 
 // deleted entry by id
-router.delete("/:id", async (req, res) => {
-  try {
-    const data = await ActivityModule.destroy({
-      where: {
-        activity_id: req.params.id,
-      },
-    });
-    if (data == 0)
-      res.status(200).json("entry not found with id: ", req.params.id);
-    else res.status.json("deleted successfully...");
-  } catch (error) {
-    res.status(500).json(error);
-  }
-});
+router.delete("/:id", );
 
 module.exports = router;
--- Server/module/Activity/ActivityService.js
+++ Server/module/Activity/ActivityService.js
@@ -3,11 +3,68 @@ const router = express.Router();
 const db = require("../../database");
 const ActivityModule = require("../module/Activity");
 
+module.exports = {
 
-module.exports={
-    getAllActivity:{},
-    getOneActivityById:{},
-    createNewActivity:{},
-    updateActivity:{},
-    deleteActivity:{}
-}
+    // get all entry
+  getAllActivity: async (req, res) => {
+    try {
+      const data = await ActivityModule.findAll();
+      res.status(200).json(data);
+    } catch (error) {
+      res.status(500).json(error);
+    }
+  },
+
+  // get One entry by id
+  getOneActivityById: async (req, res) => {
+    try {
+      const data = await ActivityModule.findByPk(req.params.id);
+      if (!data)
+        res.status(200).json("data not found with id: ", req.params.id);
+      else res.status(200).json(data);
+    } catch (error) {
+      res.status(500).json(error);
+    }
+  },
+
+  // create new antry
+  createNewActivity: async (req, res) => {
+    try {
+      const data = await ActivityModule.create(req.body);
+      res.status(201).json("new entry created successfully...");
+    } catch (error) {
+      res.status(500).json(error);
+    }
+  },
+
+  // update entry
+  updateActivity: async (req, res) => {
+    try {
+      const data = await ActivityModule.update(req.body, {
+        where: {
+          activity_id: req.params.id,
+        },
+      });
+      if (data[0] == 1) res.status(200).json("updated successfully...");
+      else res.status(200).json("already updated...");
+    } catch (error) {
+      res.status(500).json(error);
+    }
+  },
+
+  // delete entry
+  deleteActivity: async (req, res) => {
+    try {
+      const data = await ActivityModule.destroy({
+        where: {
+          activity_id: req.params.id,
+        },
+      });
+      if (data == 0)
+        res.status(200).json("entry not found with id: ", req.params.id);
+      else res.status.json("deleted successfully...");
+    } catch (error) {
+      res.status(500).json(error);
+    }
+  },
+};
