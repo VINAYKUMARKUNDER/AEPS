ceaa49b35e291c3d4d7f97e15d074b7bdfb529a0
Vinay Kumar <98196841+VINAYKUMARKUNDER@users.noreply.github.com>
2023-06-03 16:50:52 +0530

    image sorted

--- Server/module/UploadImage/Image.js
+++ Server/module/UploadImage/Image.js
@@ -1,42 +1,37 @@
 const express = require("express");
 const router = express();
-const {
-  uploadAadharBack,
-  uploadAadharFront,
-  uploadMapingImage,
-  uploadPanCardImage,
-  uploadPoliceVarificationImage,
-  uploadProfile,
-  uploadShopImage,
-} = require("./uploadImageService");
+const multer = require("multer");
 
-router.post("/profile-img/", uploadProfile, (req, res) => {
-  return res.json(req.file);
-});
+var pathName = "";
 
-router.post("/aadharf-img/", uploadAadharFront, (req, res) => {
-  return res.json(req.file);
-});
-
-router.post("/aadharb-img/", uploadAadharBack, (req, res) => {
-  return res.json(req.file);
-});
-
-router.post("/pan-img/", uploadPanCardImage, (req, res) => {
+router.post(
+  "/upload/:name",
+  multer({
+    limits: 1024 * 1024 * 5,
+    fileFilter: (req, file, done) => {
+      pathName = req.params.name;
+      if (
+        file.mimetype === "image/jpg" ||
+        file.mimetype === "image/png" ||
+        file.mimetype === "image/jpeg"
+      ) {
+        done(null, true);
+      } else {
+        done("file type not supported", false);
+      }
+    },
+    storage: multer.diskStorage({
+      destination: function (req, file, cb) {
+        cb(null, `uploads/${req.params.name}`);
+      },
+      filename: function (req, file, cb) {
+        cb(null, file.fieldname + "-" + Date.now() + ".jpg");
+      },
+    }),
+  }).single("image"),
+  (req, res) => {
     return res.json(req.file);
-});
+  }
+);
 
-router.post("/police-img/", uploadPoliceVarificationImage, (req, res) => {
-    return res.json(req.file);
-});
-
-router.post("/maping-img/", uploadMapingImage, (req, res) => {
-    return res.json(req.file);
-});
-
-router.post("/shop-img/", uploadShopImage, (req, res) => {
-    return res.json(req.file);
-});
-
-
-module.exports=router;
+module.exports = router;
